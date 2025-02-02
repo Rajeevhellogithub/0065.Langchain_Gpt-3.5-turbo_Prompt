11bfd88614c96ef7b5d15018de4d7033eeb4f6de
Rajeev Kumar <sis.rajeevsingh@gmail.com>
2025-02-02 13:14:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+# cd PrakashSenapati\2024_12_18_Langchain
+# streamlit run app_self.py
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+#from dotenv import load_dotenv
+
+os.environ["OPENAI_API_KEY"] = "xxxxxxxxxxx"
+#os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+
+os.environ["LANGCHAIN_API_KEY"]= ("yyyyyyyyyy")
+#os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt Template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","I am chatbot. I am hear to assist you. Please type your queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+
+st.title('LLM-OPENAI PROJECT')
+input_text=st.text_input("How may I help you")
+
+# openAI LLm model
+model=ChatOpenAI(model="gpt-3.5-turbo")
+output_parser=StrOutputParser()
+chain=prompt|model|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
